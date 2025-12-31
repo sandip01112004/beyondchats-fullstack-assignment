1fb648c704f7609654e32c62e309cb3a3e4ec0ed
Sandip Kharate <sandip.skharate@gmail.com>
2025-12-31 19:32:30 +0530

    Backend base API added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
+    baseURL: "https://beyondchats-fullstack-assignment-027c.onrender.com/api/v1", // Hardcoded for assignment submission as requested
     headers: {
         'Content-Type': 'application/json',
     },
